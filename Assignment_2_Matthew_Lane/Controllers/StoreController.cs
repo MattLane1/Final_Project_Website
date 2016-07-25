8c21eff8e2284f846cd6f74c943de65917df805d
mattl <mattl@DESKTOP-UV5RM65>
2016-07-25 14:49:27 -0400

    Added details view

--- Assignment_2_Matthew_Lane/Controllers/StoreController.cs
+++ Assignment_2_Matthew_Lane/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assignment_2_Matthew_Lane.Models;
 
 namespace Assignment_2_Matthew_Lane.Controllers
 {
@@ -25,11 +26,10 @@ namespace Assignment_2_Matthew_Lane.Controllers
         }
         //
         // GET: /Store/Details/5
-        public string Details(int id)
+        public ActionResult Details(int id = 1)
         {
-            string message = "Store.Details, ID = " + id;
-
-            return message;
+            var appetizer = new Appetizers { Title = "Appetizer " + id };
+            return View(appetizer);
         }
     }
 }
