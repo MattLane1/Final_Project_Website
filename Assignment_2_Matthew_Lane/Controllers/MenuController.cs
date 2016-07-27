778331b2cd1d4a21aea1c8c1a73871fc927abdb5
mattl <mattl@DESKTOP-UV5RM65>
2016-07-27 12:31:50 -0400

    Attemping to add DB.

--- Assignment_2_Matthew_Lane/Controllers/MenuController.cs
+++ Assignment_2_Matthew_Lane/Controllers/MenuController.cs
@@ -8,15 +8,40 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Assignment_2_Matthew_Lane.Models;
+using System.Collections.Generic;
 
 namespace Assignment_2_Matthew_Lane.Controllers
 {
     public class MenuController : Controller
     {
-        // GET: Contact
+        //
+        // GET: Store
         public ActionResult Index()
         {
-            return View();
+            List<FoodType> foodtypes = new List<FoodType>
+            {
+                new FoodType("Appetizers"),
+                new FoodType("Mains"),
+                new FoodType("Desserts")
+            };
+
+            return View(foodtypes);
         }
+        //
+        // GET: /Store/Browse?genre=Disco
+        public ActionResult Browse(string FoodType)
+        {
+            FoodType foodTypeModel = new FoodType { Name = FoodType };
+            return View(foodTypeModel);
+        }
+        /* 
+           //
+           // GET: /Store/Details/5
+           public ActionResult Details(int id = 1)
+           {
+               Appetizers appetizer = new Appetizers { Title = "Appetizer " + id };
+               return View(appetizer);
+           }
+        */
     }
 }
