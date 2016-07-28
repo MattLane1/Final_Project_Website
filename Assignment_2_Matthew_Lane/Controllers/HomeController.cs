d4ea9e8b80c2c38865b34ccf0732704de0630ac9
mattl <mattl@DESKTOP-UV5RM65>
2016-07-28 15:42:06 -0400

    Completed the Home page

--- Assignment_2_Matthew_Lane/Controllers/HomeController.cs
+++ Assignment_2_Matthew_Lane/Controllers/HomeController.cs
@@ -15,19 +15,7 @@ namespace Assignment_2_Matthew_Lane.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
 
     }
 }
